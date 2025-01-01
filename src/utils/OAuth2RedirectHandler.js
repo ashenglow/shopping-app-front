c0134f9740df47ddf3f0c78f14fca2293d8ae5b1
ashenglow <158246901+ashenglow@users.noreply.github.com>
2025-01-01 15:07:53 +0900

    refactor: simplify OAuth2 auth flow
    - Remove duplicate code for handling OAuth2 callback parameters
    - Consolidate OAuth2 success handling logic
    - Clean up unnecessary state management code

--- src/utils/OAuth2RedirectHandler.js
+++ src/utils/OAuth2RedirectHandler.js
@@ -1,40 +1,30 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { useHistory, useLocation } from "react-router-dom";
 import { CircularProgress } from "@mui/material";
 import { Styledcontainer } from "../component/layout/MUI-comp/MuiStyles"; 
 import { handleOAuth2Success } from "../actions/oauth2Action";
 
 const OAuth2RedirectHandler = () => {
-    const location = useLocation();
     const dispatch = useDispatch();
-    const history = useHistory();
 
     useEffect(() => {
         const handleOAuth2Redirect = async () => {
-        const params = new URLSearchParams(window.location.search);
-        const token = params.get("token");
-        const userId = params.get("userId");
-        const userName = params.get("nickname");
-        const error = params.get("error");
-
-        if (token) {
-            try{
-                await dispatch(handleOAuth2Success(token, userId, userName));
-                history.push("/");     
+            const hash = window.location.hash;
+            if (hash.includes("oauth2/callback")) {
+                const params = new URLSearchParams(hash.split("?")[1]); 
+                const token = params.get("token");
+                const userId = params.get("userId");
+                const userName = params.get("nickname");
+                if (token && userId && userName) {
+                    await dispatch(handleOAuth2Success(token, userId, userName));
+                    window.location.href = "/";
+                }
+                
             }
-            catch(error){
-                console.error("OAuth2 handling failed: ", error);
-                history.push("/login");
-            }
-        } else if (error) {
-           console.error("OAuth2 error: ", error);
-            history.push("/login");
-        }
     };
 
     handleOAuth2Redirect();
-    }, [history]);
+    }, [dispatch]);
     return (
         <Styledcontainer>
             <CircularProgress />
