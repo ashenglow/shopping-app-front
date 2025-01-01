0ffa36f957e65e87fa4cb57ba133f71a65472cc4
ashenglow <158246901+ashenglow@users.noreply.github.com>
2025-01-01 18:06:45 +0900

    Update OAuth2RedirectHandler.js

--- src/utils/OAuth2RedirectHandler.js
+++ src/utils/OAuth2RedirectHandler.js
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useHistory } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { CircularProgress } from "@mui/material";
 import { Styledcontainer } from "../component/layout/MUI-comp/MuiStyles"; 
@@ -8,35 +8,27 @@ import { handleOAuth2Success } from "../actions/oauth2Action";
 const OAuth2RedirectHandler = () => {
     const dispatch = useDispatch();
     const history = useHistory();
+    const location = useLocation();
 
     useEffect(() => {
-            const hash = window.location.hash;
-            if (hash.includes("oauth2/callback")) {
-                const params = new URLSearchParams(hash.split("?")[1]); 
-                
-                const token = params.get("token");
-                const userId = params.get("userId");
-                const userName = params.get("nickname");
+        const params = new URLSearchParams(location.search);
+        const token = params.get("token");
+        const userId = params.get("userId");
+        const userName = params.get("nickname");
 
-                if (token && userId) {
-                  dispatch(handleOAuth2Success(token, userId, userName))
-                  .then(() => {
-                      window.history.replaceState(null, null, "/");
-                      history.replace("/");
-                  })
-                  .catch((error) => {
-                      console.log(error);
-                      history.replace("/login");
-                  });
-                }else{
+        if (token && userId) {
+            dispatch(handleOAuth2Success(token, userId, userName))
+                .then(() => {
+                    history.replace("/");
+                })
+                .catch((error) => {
+                    console.error(error);
                     history.replace("/login");
-                }
-                
-            }
-   
-
-
-    }, [dispatch, history]);
+                });
+        } else {
+            history.replace("/login");
+        }
+    }, [dispatch, history, location]);
     return (
         <Styledcontainer>
             <CircularProgress />
